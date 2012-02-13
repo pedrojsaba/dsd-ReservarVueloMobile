5a3a90a687372889e0ab4f758b1b298cbf265b65
demogithub <pedro.jancachagua@gmail.com>
2012-02-12 21:25:32 -0500

    eliminando imagen no utilizada

--- app/controllers/sessions_controller.rb
+++
@@ -1,8 +0,0 @@
-class SessionsController < ApplicationController
-  def create
-    auth = request.env['rack.auth']
-    Rails.logger.info auth.inspect
-    self.current_user = Authorization.find_or_create_from_hash(auth)
-    render :text => "Welcome, #{current_user.name}."
-  end
-end
--- config/initializers/omniauth.rb
+++
@@ -1,3 +0,0 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['147507248699725'], ENV['84388faa031550cc015d39191a32f513']
-end
